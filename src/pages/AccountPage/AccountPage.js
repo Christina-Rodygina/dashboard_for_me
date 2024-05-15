f26e70c52e521ae9382a1bbdd672ac35c57c5882
Christina-Rodygina <kristinarodygina@mail.ru>
2024-05-15 15:07:32 +0300

    Added layout for editing account data (without css)

--- src/pages/AccountPage/AccountPage.js
+++ src/pages/AccountPage/AccountPage.js
@@ -8,6 +8,7 @@ const AccountPage = () => {
     const [telegram, setTelegram] = useState(String)
     const [dateReg, setDateReg] = useState(String)
     const [username, setUsername] = useState(String)
+    const [edit, setEdit] = useState(false)
 
     const me = async () => {
         try {
@@ -48,45 +49,69 @@ const AccountPage = () => {
                             <div className="account__column left">
                                 <img src="/account-logo.png" alt="Account Logo"
                                      className="account__logo"/>
-                                <span>{username}</span>
-
-
+                                {edit ? (
+                                    <div className="username-edit-input">
+                                        <label className="username-edit-input__label">User Name</label>
+                                        <input type="text" autoComplete="off"/>
+                                    </div>
+                                ) : (
+                                    <span>{username}</span>
+                                )}
                                 <div className="account__notification">
                                     <span className="account__ntf-text">Notification</span>
                                     <div className="">
                                         <label className="toggler-wrapper style-1">
-                                            <input type="checkbox" />
+                                            <input type="checkbox"/>
                                             <div className="toggler-slider">
                                                 <div className="toggler-knob"></div>
                                             </div>
                                         </label>
                                     </div>
                                 </div>
-
-
-                                <button className="account__edit-btn">
+                                <button onClick={() => setEdit(true)} className="account__edit-btn">
                                     <img src="/edit-svgrepo-com.svg" alt="Edit"/>
                                 </button>
                             </div>
                             <div className="account__column right">
                                 <h3>Information</h3>
                                 <div className="account__info">
-                                    <div className="account__email">
-                                        <h4>Email</h4>
-                                        <span>{email}</span>
-                                    </div>
-                                    <div className="account__pass">
-                                        <h4>Password</h4>
-                                        <span style={{fontSize: "20px"}}>·········</span>
-                                    </div>
-                                    <div className="account__telegram">
-                                        <h4>Telegram Account</h4>
-                                        <span>{telegram ? telegram : "unspecified"}</span>
-                                    </div>
-                                    <div className="account__date-reg">
-                                        <h4>Date of registration</h4>
-                                        <span>{dateReg}</span>
-                                    </div>
+                                    {edit ? (
+                                        <>
+                                            <div className="email-edit-input">
+                                                <label className="email-edit-input__label" htmlFor="email">Email</label>
+                                                <input type="email" autoComplete="email" value={email}/>
+                                            </div>
+                                            <div className="pass-edit-input">
+                                                <label className="pass-edit-input__label" htmlFor="password">New
+                                                    Password</label>
+                                                <input type="password" autoComplete="off" value="·········"/>
+                                            </div>
+                                            <div className="telegram-edit-input">
+                                                <label className="telegram-edit-input__label">Telegram Account</label>
+                                                <input type="text" autoComplete="off" value={telegram}/>
+                                            </div>
+                                            <button className="edit-save-input">Save</button>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <div className="account__email">
+                                                <h4>Email</h4>
+                                                <span>{email}</span>
+                                            </div>
+                                            <div className="account__pass">
+                                                <h4>Password</h4>
+                                                <span style={{fontSize: "20px"}}>·········</span>
+                                            </div>
+                                            <div className="account__telegram">
+                                                <h4>Telegram Account</h4>
+                                                <span>{telegram ? telegram : "unspecified"}</span>
+                                            </div>
+                                            <div className="account__date-reg">
+                                                <h4>Date of registration</h4>
+                                                <span>{dateReg}</span>
+                                            </div>
+                                        </>
+                                    )}
                                 </div>
                             </div>
                         </div>
